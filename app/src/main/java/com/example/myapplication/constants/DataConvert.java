5c773a2c5001218a6abcddde843ce725c8e0bd88
Bob <932458047@qq.com>
2025-02-21 18:54:46 +0800

    解决读取数据为负数的问题

--- app/src/main/java/com/example/myapplication/constants/DataConvert.java
+++ app/src/main/java/com/example/myapplication/constants/DataConvert.java
@@ -10,10 +10,11 @@ import java.nio.charset.StandardCharsets;
 public class DataConvert {
 
     public static Object convert2Obj(byte[] b, int valType) {
-        try{
+        try {
             if (b != null && b.length > 0) {
                 switch (valType) {
                     case 0:
+                        return ByteUtil.byte2int(new byte[]{0, 0, b[1], b[0]});
                     case 1:
                         return ByteUtil.byte2short(new byte[]{b[1], b[0]});
                     case 2:
@@ -22,14 +23,14 @@ public class DataConvert {
 //                            LogUtil.error("非法参数：b==>"+ByteUtil.bytes2HexString(b)+"  valType==>"+valType);
 //                        }
 //                        return ByteUtil.byte2int(ByteUtil.reverse(b));
-                        return ByteUtil.byte2int(new byte[]{b[3],b[2],b[1],b[0]});
+                        return ByteUtil.byte2int(new byte[]{b[3], b[2], b[1], b[0]});
                     case 4:
                         return null;
                     case 5:
                         return new String(b, StandardCharsets.US_ASCII).trim();
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
